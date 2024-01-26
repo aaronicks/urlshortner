f6e129f52853582dbe18d5f8934f3a9d71150806
Solanke Afolabi <aaronicks@gmail.com>
2024-01-26 11:46:16 +0100

    final commit

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "urlshortner.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": dj_database_url.config()
-# }
+DATABASES = {
+    'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3'
+    }
+}
+
 
 
 # Password validation
